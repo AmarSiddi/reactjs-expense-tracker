c04b3087433c570090c6e86ae400d8f3a2696710
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-18 04:05:42 +0100

    modified App.js files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>Hello from Amplify<h1>
+        <h1>Hello from Amplify</h1>
         
         <a
           className="App-link"
