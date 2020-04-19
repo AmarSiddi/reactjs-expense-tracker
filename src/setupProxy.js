0f8dce962d2b06892c2d72ca856a477ea265758f
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-19 15:17:49 +0100

    modified proxy setting for prod and resolved proxy issues

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 //const target_url = 'http://18.207.32.33'
+//"proxy": "http://18.207.32.33",    //usage only in dev environment, put this field in packate.jason before dependencies section
 module.exports = function(app) {
   app.use('/api/auth/',createProxyMiddleware({
       target: process.env.REACT_APP_HOST_URL,
